b9839aa0195d681e33ed23d0e02b1c4e9e1eb0ab
sgndm <ransika@gmail.com>
2018-06-07 19:55:03 +0530

    completed

--- src/app/pages/customers/customer/customer.component.ts
+++ src/app/pages/customers/customer/customer.component.ts
@@ -10,6 +10,20 @@ export class CustomerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      $('#tab_1').show();
+  }
+
+  show_tab(tab_id, count) {
+      // alert(tab_id);
+      $('#' + tab_id).show();
+      for(let x = 1; x <= count; x++) {
+          if(x == tab_id) {
+              $('#tab_' + x).show();
+          } else {
+              $('#tab_' + x).hide();
+          }
+
+      }
   }
 
 }
